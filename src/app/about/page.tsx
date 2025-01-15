5cf914c60c832816dfa929b65cd179b3ea2dfa82
Evan CHARRIER <evanc378549@gmail.com>
2025-01-15 15:39:20 +0100

    Revert "test linkedin badge"
    This reverts commit a7a86ff9c68594c9c2e0850051d34817d4f33cdb.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,35 +1,3 @@
-"use client";
-
-import Link from "next/link";
-import Script from "next/script";
-
-export default function AboutPage() {
-  return (
-    <div>
-      <h1>À propos</h1>
-
-      <Script
-        src="https://platform.linkedin.com/badges/js/profile.js"
-        strategy="lazyOnload"
-        onLoad={() => console.log("LinkedIn Badge script loaded successfully.")}
-      />
-
-      <div
-        className="badge-base LI-profile-badge"
-        data-locale="fr_FR"
-        data-size="large"
-        data-theme="dark"
-        data-type="HORIZONTAL"
-        data-vanity="evancharrier"
-        data-version="v1"
-      >
-        <Link
-          className="badge-base__link LI-simple-link"
-          href="https://fr.linkedin.com/in/evancharrier?trk=profile-badge"
-        >
-          Evan Charrier
-        </Link>
-      </div>
-    </div>
-  );
+export default function AboutPage () {
+    return <h1>A propos</h1>
 }
