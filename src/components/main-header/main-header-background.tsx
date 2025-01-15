dcd0018d0968fac9979dbcc117afb872a6969b05
Evan CHARRIER <evanc378549@gmail.com>
2025-01-15 14:50:37 +0100

    fix navlink color

--- src/components/main-header/main-header-background.tsx
+++ src/components/main-header/main-header-background.tsx
@@ -4,7 +4,7 @@ export default function MainHeaderBackground() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
           <defs>
             <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset="0%" style={{ stopColor: "#05a3ff", stopOpacity: "1" }} />
+              <stop offset="0%" style={{ stopColor: "#3a4e8f", stopOpacity: "1" }} />
               <stop offset="100%" style={{ stopColor: "#3155cb", stopOpacity: "1" }} />
             </linearGradient>
           </defs>
--- src/components/main-header/nav-link.tsx
+++ src/components/main-header/nav-link.tsx
@@ -17,8 +17,8 @@ export default function NavLink({ href, children }: NavLinkProps) {
     <Link
       href={href}
       className={` font-bold py-2 px-4 rounded-md 
-        hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-[#05a3ff] hover:to-[#3155cb] hover:shadow-[0_0_18px_rgba(3,163,255,0.8)] 
-        ${isActive ? "bg-clip-text text-transparent bg-gradient-to-r from-[#05a3ff] to-[#3155cb]" : ""}`}
+        hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-[#05a3ff] hover:to-[#0c8bd4] hover:[text-shadow:0_0_18px_rgba(3,163,255,0.8)] 
+        ${isActive ? "bg-clip-text text-transparent bg-gradient-to-r from-[#05a3ff] to-[#0c8bd4] [text-shadow:0_0_18px_rgba(3,163,255,0.8)]" : ""}`}
     >
       {children}
     </Link>
