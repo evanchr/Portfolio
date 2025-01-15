cafc2f6d3b07fdb86ad6ea3c86a37c35016f74be
Evan CHARRIER <evanc378549@gmail.com>
2025-01-15 14:20:00 +0100

    add all projects

--- src/app/projets/page.tsx
+++ src/app/projets/page.tsx
@@ -20,6 +20,21 @@ export default function ProjetsPage() {
             Voir MovieList
           </Link>
         </li>
+        <li>
+          <Link href="https://tasks.evancharrier.fr" target="_blank">
+            Voir Tasks
+          </Link>
+        </li>
+        <li>
+          <Link href="https://next-news.evancharrier.fr" target="_blank">
+            Voir NextNews
+          </Link>
+        </li>
+        <li>
+          <Link href="https://reactweet.evancharrier.fr" target="_blank">
+            Voir ReacTweet
+          </Link>
+        </li>
       </ul>
     </div>
   );
